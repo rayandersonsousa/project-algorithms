abb975c2203ba294e35977af9ec9396fb24f4c4d
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 17:41:58 -0300

    fix: Corrigindo resultados esperados dos testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,10 +4,10 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     # Teste para a criptografia de chave par
-    assert encrypt_message("luffy", 2) == "ffy_ul"
+    assert encrypt_message("luffy", 2) == "yff_ul"
 
     # Teste para a criptografia de chave ímpar
-    assert encrypt_message("luffy", 3) == "yff_ul"
+    assert encrypt_message("luffy", 3) == "ful_yf"
 
     # Teste para a criptografia de chave inválida
     assert encrypt_message("luffy", 56) == "yfful"
