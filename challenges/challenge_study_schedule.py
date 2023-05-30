876030b7d2edc41b1b59417db8c29c9a3dfd7811
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 16:41:00 -0300

    feat: Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui. Initial commit"""
-    """Tentando criar o pull request"""
-    raise NotImplementedError
+  try:
+    students = 0
+    for start_time, end_time in permanence_period:
+      if start_time <= target_time <= end_time:
+        students += 1
+  
+  except(TypeError):
+    return None
+  
+  return students
