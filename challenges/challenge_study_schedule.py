5b478f5d72eca88703bb72feacc14288086299cc
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 17:34:42 -0300

    fix: Corrigindo erros de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,8 @@ def study_schedule(permanence_period, target_time):
     try:
         students = 0
         for start_time, end_time in permanence_period:
-        if start_time <= target_time <= end_time:
-            students += 1
+            if start_time <= target_time <= end_time:
+                students += 1
 
     except (TypeError):
         return None
