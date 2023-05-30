57ff2a6946f3d7fdd91fe1ff6cc613cb00915bc5
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 17:51:15 -0300

    feat: Requisito 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    elif low_index > high_index:
+        return True
+    elif word[high_index] == word[low_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
