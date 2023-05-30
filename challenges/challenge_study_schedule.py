2486ec736adbd6327755157b74566218e3b80633
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 16:10:40 -0300

    test: Pullrequest

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,4 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    """Faça o código aqui. Initial commit"""
+    """Tentando criar o pull request"""
     raise NotImplementedError
