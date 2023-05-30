ba1efaf396aa2e6ec79a59a8c7a87b5f94092fea
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-05-30 17:31:51 -0300

    Requisito 02 e correção de erros de lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pyteste
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # Teste para a criptografia de chave par
+    assert encrypt_message("luffy", 2) == "ffy_ul"
+
+    # Teste para a criptografia de chave ímpar
+    assert encrypt_message("luffy", 3) == "yff_ul"
+
+    # Teste para a criptografia de chave inválida
+    assert encrypt_message("luffy", 56) == "yfful"
+
+    # Teste para a criptografia de chave inválida e string vazia
+    assert encrypt_message("", 56) == ""
+
+    # Teste para a criptografia com parametros incorretos
+    with pyteste.raises(TypeError):
+        encrypt_message(56, "luffy")
